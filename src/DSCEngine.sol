117d29c0142fb0b7a46ee3c08cddc2413a67706a
motypes <mohammed.jalil@gmail.com>
2023-08-31 16:08:41 +0800

    wip adding deposit function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -3,8 +3,8 @@
 // Layout of Contract:
 // version
 // imports
-// errors
 // interfaces, libraries, contracts
+// errors
 // Type declarations
 // State variables
 // Events
@@ -23,6 +23,9 @@
 
 pragma solidity ^0.8.18;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 /**
  * @title Decentralized Stable Coin Engine
  * @author @motypes
@@ -36,10 +39,79 @@ pragma solidity ^0.8.18;
  * Our DSC system should always be "overcollateralized". At no point should the value of the collateral be less than the dollar value of the DSC.
  * @notice This contract is the core of the DSC system. It handles all the logic for minting and redeming DSC, as well as depositing & withdrawing collateral.
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
+    // Errors
+    error DSCEngine__MustBeMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine__NotAllowedToken();
+
+
+    // State Variables
+    mapping(address token => address s_priceFeeds) public s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    // Modifiers
+    modifier moreThanZero(uint256 _amount){
+        if(_amount <= 0) {
+            revert DSCEngine__MustBeMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if(s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    // Functions
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        // We will be using USD pricefeeds for all tokens
+        if(tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        // E.g. ETH/USD, BTC/USD, LINK/USD, etc.
+        for(uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+
+    }
+
+    // External Functions
     function depositCollateralAndMintDsc() external {}
 
-    function depositCollateral() external {}
+    /**
+     * 
+     * @param tokenCollateralAddress The address of the token to deposit
+     * @param amountCollateral The amount of the token to deposit
+     */
+    function depositCollateral( address tokenCollateralAddress, uint256 amountCollateral) 
+        external 
+        moreThanZero(amountCollateral) 
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant {
+        // Transfer the tokens from the user to this contract
+        // Approve the transfer first
+        // Transfer the tokens
+        // Check the balance of the contract
+        // If the balance is greater than the amount of collateral, mint DSC
+        // If the balance is less than the amount of collateral, revert
+        // If the balance is equal to the amount of collateral, do nothing
+    }
+
+
+    }
+
 
     function redeemCollateralForDsc() external {}
 
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -3,8 +3,8 @@
 // Layout of Contract:
 // version
 // imports
-// errors
 // interfaces, libraries, contracts
+// errors
 // Type declarations
 // State variables
 // Events
@@ -37,20 +37,20 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is meant to be governed by DSCEngine. This contract is just the ERC20 implemention of our Stable coin system.
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
-    error DecentralizedStableCoin_MustBeMoreThanZero();
-    error DecentralizedStableCoin_BurnAmountExceedsBalance();
-    error DecentralizedStableCoin_NotZeroAddress();
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("Decentralized Stable Coin", "DSC") {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert DecentralizedStableCoin_MustBeMoreThanZero();
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
 
         if (balance < _amount) {
-            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
 
         super.burn(_amount);
@@ -61,11 +61,11 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
         uint256 _amount
     ) external onlyOwner returns (bool) {
         if (_to == address(0)) {
-            revert DecentralizedStableCoin_NotZeroAddress();
+            revert DecentralizedStableCoin__NotZeroAddress();
         }
 
         if (_amount <= 0) {
-            revert DecentralizedStableCoin_MustBeMoreThanZero();
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
 
         _mint(_to, _amount);
