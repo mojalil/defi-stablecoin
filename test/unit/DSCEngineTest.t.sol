98f7cdfe4b1b35bd21f0258665c2c78a69c7b13b
motypes <mohammed.jalil@gmail.com>
2023-10-06 16:13:54 +0800

    added test for reverting when collateral is zero

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,15 +16,17 @@ contract DSCEngineTest is Test {
     HelperConfig config;
     address ethUsdPriceFeed;
     address weth;
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
-        (ethUsdPriceFeed, ,weth, , ) = config.activeNetworkConfig();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
     }
 
     // Price Test
-    function testGetUsdValue() public{
+    function testGetUsdValue() public {
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
@@ -31,4 +34,15 @@ contract DSCEngineTest is Test {
         assertEq(expectedUsd, actualUsd);
     }
 
+    // Deposit Collateral test
+    function testRevertIfCollateralIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMoreThanZero.selector);
+
+        dsce.depositCollateral(weth, 0);
+
+        vm.stopPrank();
+    }
 }
